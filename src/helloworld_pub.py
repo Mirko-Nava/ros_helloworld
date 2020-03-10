85a4ff8e7820d56a8b1a2725395d4d4080209d86
Mirko Nava <mirko@idsia.ch>
2020-03-10 13:52:21 +0100

    exercise template for publish and subscriber architecture

--- src/helloworld_pub.py
+++ src/helloworld_pub.py
@@ -10,38 +10,18 @@ class HelloworldPub:
         """Initialization."""
 
         # initialize the node
-        rospy.init_node(
-            'helloworld_pub'  # name of the node
-        )
 
         self.name = '<your_name_here>'
 
         # create string publisher
-        self.publisher = rospy.Publisher(
-            '/helloworld',  # name of the topic
-            String,  # message type
-            queue_size=10  # queue size
-        )
 
         # set node update frequency in Hz
-        self.rate = rospy.Rate(10)
 
     def publish(self):
         """Publishes a message."""
-
-        while not rospy.is_shutdown():
-
-            # publish the message
-            self.publisher.publish(self.name)
-
-            # sleep until next step
-            self.rate.sleep()
 
 
 if __name__ == '__main__':
     publisher = HelloworldPub()
 
-    try:
-        publisher.publish()
-    except rospy.ROSInterruptException as e:
-        pass
+    publisher.publish()
--- src/helloworld_sub.py
+++ src/helloworld_sub.py
@@ -10,37 +10,19 @@ class HelloworldSub:
         """Initialization."""
 
         # initialize the node
-        rospy.init_node(
-            'helloworld_sub'  # name of the node
-        )
 
         # create string subscriber
-        self.subscriber = rospy.Subscriber(
-            '/helloworld',  # name of the topic
-            String,  # message type
-            self.greet  # function that handles incoming messages
-        )
 
         # set node update frequency in Hz
-        self.rate = rospy.Rate(10)
 
     def greet(self, data):
         """Greets the person."""
-        rospy.loginfo_once('Hello %s!' % data.data)
 
     def wait_for_termination(self):
         """Waits until the node is terminated."""
-
-        while not rospy.is_shutdown():
-
-            # sleep until next step
-            self.rate.sleep()
 
 
 if __name__ == '__main__':
     subscriber = HelloworldSub()
 
-    try:
-        subscriber.wait_for_termination()
-    except rospy.ROSInterruptException as e:
-        pass
+    subscriber.wait_for_termination()
